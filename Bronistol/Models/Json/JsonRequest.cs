1bb2bf99b0b812c1df036d6ac9ebf5dced430b2b
n.korabelnikov@isoit.ru <n.korabelnikov@isoit.ru>
2021-03-15 19:41:13 +0300

    Controller entities

---
+++ Bronistol/Models/Json/JsonRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bronistol.Models.Json
+{
+    public class JsonRequest<T>
+    {
+        public T Item { get; set; }
+        public DateTime RequestTime { get; set; }
+        public JsonRequest()
+        {
+            RequestTime = DateTime.UtcNow;
+        }
+    }
+}
---
+++ Bronistol/Models/Json/JsonResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bronistol.Models.Json
+{
+    public class JsonResponse<T>
+    {
+        public T Item { get; set; }
+        public DateTime ResponseTime { get; set; }
+        public JsonResponse()
+        {
+            ResponseTime = DateTime.UtcNow;
+        }
+    }
+}
