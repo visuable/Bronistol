78f6ea4b91bda9095352316e6352341282ba590c
n.korabelnikov@isoit.ru <n.korabelnikov@isoit.ru>
2021-03-17 19:54:49 +0300

    Moved IncludingExtensions

---
+++ Bronistol.Database/Extensions/IncludingExtensions.cs
@@ -0,0 +1,26 @@
+﻿
+using Bronistol.Database.DbEntities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bronistol.Database.Extensions
+{
+    public static class IncludingExtensions
+    {
+        public static IIncludableQueryable<BookingEntity, DbEntity> IncludeBookingEntity(this DbSet<BookingEntity> bookingEntities)
+        {
+            return bookingEntities
+                .Include(x => x.AssignedDate)
+                .Include(x => x.SubmitDate)
+                .Include(x => x.Reason)
+                .Include(x => x.Priority)
+                .Include(x => x.Note)
+                .Include(x => x.Name);
+        }
+    }
+}
