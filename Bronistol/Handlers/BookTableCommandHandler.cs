507020d3e91f7fbaff1de40b1e447fef7d707952
n.korabelnikov@isoit.ru <n.korabelnikov@isoit.ru>
2021-03-25 19:46:29 +0300

    MediatR handlers

---
+++ Bronistol/Handlers/BookTableCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Bronistol.Commands;
+using Bronistol.Core.Supports;
+using Bronistol.Database.EntitiesDto;
+using Bronistol.Database.Enumerations;
+using Bronistol.Models.Responses;
+using Bronistol.Options;
+using Bronistol.Validators;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.Options;
+
+namespace Bronistol.Handlers
+{
+    public class BookTableCommandHandler : IRequestHandler<Commands.BookTableRequest, DefaultResponse>
+    {
+        private BookTableRequestValidator _bookTableRequestValidator;
+        private IBookingSupport _bookingSupport;
+        private IMapper _mapper;
+        public BookTableCommandHandler(BookTableRequestValidator bookTableRequestValidator, IBookingSupport bookingSupport, IMapper mapper)
+        {
+            _bookTableRequestValidator = bookTableRequestValidator;
+            _bookingSupport = bookingSupport;
+            _mapper = mapper;
+        }
+
+        public async Task<DefaultResponse> Handle(Commands.BookTableRequest request, CancellationToken cancellationToken)
+        {
+            var result = await _bookTableRequestValidator.ValidateAsync(request, CancellationToken.None);
+            if (!result.IsValid) return new DefaultResponse() {Response = false};
+            await _bookingSupport.AddBookingEntity(_mapper.Map<BookingEntityDto>(request.Table));
+            return new DefaultResponse() {Response = true};
+
+        }
+    }
+}
---
+++ Bronistol/Handlers/GetReservedTablesCommandRequestHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Bronistol.Database.DbEntities;
+using Bronistol.Database.EntitiesDto;
+using Bronistol.Database.Repositories;
+using Bronistol.Models;
+using Bronistol.Models.Responses;
+using Bronistol.Requests;
+using Bronistol.Validators;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bronistol.Handlers
+{
+    public class GetReservedTablesCommandRequestHandler : IRequestHandler<GetReservedTablesCommand, Response<List<BookingEntityViewModel>>>
+    {
+        private IRepository<BookingEntity> _bookingEntityRepository;
+        private GetReservedTablesCommandValidator _getReservedTablesCommandValidator;
+        private IMapper _mapper;
+
+        public GetReservedTablesCommandRequestHandler(IRepository<BookingEntity> bookingEntityRepository, GetReservedTablesCommandValidator getReservedTablesCommandValidator, IMapper mapper)
+        {
+            _bookingEntityRepository = bookingEntityRepository;
+            _getReservedTablesCommandValidator = getReservedTablesCommandValidator;
+            _mapper = mapper;
+        }
+
+        public async Task<Response<List<BookingEntityViewModel>>> Handle(GetReservedTablesCommand request, CancellationToken cancellationToken)
+        {
+            var validationResult = await _getReservedTablesCommandValidator.ValidateAsync(request, cancellationToken);
+            if (!validationResult.IsValid) throw new Exception("Request is invalid");
+            var all = await _bookingEntityRepository.GetAllAsync();
+            var count = request.Count + request.Offset * request.Count;
+            var items = all.Take(count);
+            var itemsDto = _mapper.ProjectTo<BookingEntityDto>(items);
+            var itemsViewModel = _mapper.ProjectTo<BookingEntityViewModel>(itemsDto);
+            return new Response<List<BookingEntityViewModel>>()
+            {
+                Item = await itemsViewModel.ToListAsync(cancellationToken)
+            };
+        }
+    }
+}
