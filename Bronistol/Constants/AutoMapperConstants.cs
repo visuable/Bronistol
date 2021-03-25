db5d37a1ddf72589e1bd828ae86251ee5d91f4dc
n.korabelnikov@isoit.ru <n.korabelnikov@isoit.ru>
2021-03-25 19:46:01 +0300

    Add automapper constants

---
+++ Bronistol/Constants/AutoMapperConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bronistol.Constants
+{
+    public class AutoMapperConstants
+    {
+        public const string DateTimeFormat = "yyyy-MM-dd HH:mm";
+    }
+}
