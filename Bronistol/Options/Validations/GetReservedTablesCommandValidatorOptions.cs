dd560d61fa121da21a54a058bebc9b6d69e97a5e
n.korabelnikov@isoit.ru <n.korabelnikov@isoit.ru>
2021-03-25 19:43:35 +0300

    Added options

---
+++ Bronistol/Options/Validations/GetReservedTablesCommandValidatorOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bronistol.Options.Validations
+{
+    public class GetReservedTablesCommandValidatorOptions : ValidationOptions
+    {
+        public int MaxCount { get; set; }
+    }
+}
---
+++ Bronistol/Options/Validations/ValidationOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bronistol.Options
+{
+    public class ValidationOptions
+    {
+        public string DefaultMessage { get; set; }
+    }
+}
