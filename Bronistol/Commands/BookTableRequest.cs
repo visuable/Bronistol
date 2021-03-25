d94ba78d63b9d706f3248dc945b648d2e67dd4ce
n.korabelnikov@isoit.ru <n.korabelnikov@isoit.ru>
2021-03-25 19:45:21 +0300

    Requests and commands

---
+++ Bronistol/Commands/BookTableRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bronistol.Database.EntitiesDto;
+using Bronistol.Database.Enumerations;
+using Bronistol.Models;
+using Bronistol.Models.Responses;
+using MediatR;
+
+namespace Bronistol.Commands
+{
+    public class BookTableRequest : IRequest<DefaultResponse>
+    {
+        public BookingEntityViewModel Table { get; set; }
+    }
+}
---
+++ Bronistol/Requests/GetReservedTablesCommand.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Bronistol.Models;
+using Bronistol.Models.Responses;
+using MediatR;
+
+namespace Bronistol.Requests
+{
+    public class GetReservedTablesCommand : IRequest<Response<List<BookingEntityViewModel>>>
+    {
+        public int Count { get; set; }
+        public int Offset { get; set; }
+    }
+}
