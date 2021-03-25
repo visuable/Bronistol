16f81e7dc83c73285289ad820e46e37c8009d266
n.korabelnikov@isoit.ru <n.korabelnikov@isoit.ru>
2021-03-25 20:36:43 +0300

    Added automapper extension

---
+++ Bronistol.Core/Extensions/AutoMapperExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using AutoMapper;
+
+namespace Bronistol.Core.Extensions
+{
+    public static class AutoMapperExtensions
+    {
+        public static List<TOutput> MapList<TOutput, TInput>(this IMapper mapper, List<TInput> inputList)
+        {
+            var output = new List<TOutput>();
+            foreach (var input in inputList)
+            {
+                output.Add(mapper.Map<TOutput>(input));
+            }
+
+            return output;
+        }
+    }
+}
