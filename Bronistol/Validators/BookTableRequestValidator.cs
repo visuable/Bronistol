40258d4ec9763b242492178e554f25917454ad6d
n.korabelnikov@isoit.ru <n.korabelnikov@isoit.ru>
2021-03-25 19:45:05 +0300

    Added validators

---
+++ Bronistol/Validators/BookTableRequestValidator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Bronistol.Commands;
+using Bronistol.Constants;
+using Bronistol.Database.EntitiesDto;
+using Bronistol.Database.Enumerations;
+using Bronistol.Options;
+using FluentValidation;
+using Microsoft.Extensions.Options;
+
+namespace Bronistol.Validators
+{
+    public class BookTableRequestValidator : AbstractValidator<BookTableRequest>
+    {
+        private IOptions<ValidationOptions> _validationOptions;
+        public BookTableRequestValidator(IOptions<ValidationOptions> validationOptions)
+        {
+            _validationOptions = validationOptions;
+            var defaultMessage = _validationOptions.Value.DefaultMessage;
+
+            RuleFor(x => x.Table.Name)
+                .Must(x => !string.IsNullOrWhiteSpace(x)).WithMessage(defaultMessage);
+            RuleFor(x => x.Table.Reason)
+                .Must(x => !string.IsNullOrWhiteSpace(x)).WithMessage(defaultMessage);
+            RuleFor(x => x.Table.Priority)
+                .Must(x => Enum.GetNames(typeof(Priority)).Contains(x)).WithMessage(defaultMessage);
+            RuleFor(x => x.Table.SubmitDate)
+                .Must(x => DateTime.TryParseExact(x, AutoMapperConstants.DateTimeFormat, CultureInfo.InvariantCulture,
+                    DateTimeStyles.AllowWhiteSpaces, out _));
+            RuleFor(x => x.Table.AssignedDate)
+                .Must(x => DateTime.TryParseExact(x, AutoMapperConstants.DateTimeFormat, CultureInfo.InvariantCulture,
+                    DateTimeStyles.AllowWhiteSpaces, out _));
+        }
+    }
+}
---
+++ Bronistol/Validators/GetReservedTablesCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bronistol.Options.Validations;
+using Bronistol.Requests;
+using FluentValidation;
+using Microsoft.Extensions.Options;
+
+namespace Bronistol.Validators
+{
+    public class GetReservedTablesCommandValidator : AbstractValidator<GetReservedTablesCommand>
+    {
+        private IOptions<GetReservedTablesCommandValidatorOptions> _options;
+        public GetReservedTablesCommandValidator(IOptions<GetReservedTablesCommandValidatorOptions> options)
+        {
+            _options = options;
+            RuleFor(x => x.Count)
+                .LessThanOrEqualTo(_options.Value.MaxCount).GreaterThan(0);
+            RuleFor(x => x.Offset)
+                .GreaterThanOrEqualTo(0);
+        }
+    }
+}
