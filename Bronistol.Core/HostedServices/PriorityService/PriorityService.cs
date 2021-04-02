ab398b2d97b8bb570c35dea0cb6b734a73fcb41e
n.korabelnikov@isoit.ru <n.korabelnikov@isoit.ru>
2021-04-02 15:38:51 +0300

    Added auto cleaning

--- Bronistol.Core/HostedServices/PriorityService/PriorityService.cs
+++ Bronistol.Core/HostedServices/PriorityService/PriorityService.cs
@@ -1,10 +1,13 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Bronistol.Core.Extensions;
+using Bronistol.Core.Options;
 using Bronistol.Database.DbEntities;
 using Bronistol.Database.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 
 namespace Bronistol.Core.HostedServices.PriorityService
 {
@@ -20,11 +23,13 @@ namespace Bronistol.Core.HostedServices.PriorityService
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             var bookingEntityRepository = _serviceScopeFactory.GetServiceFromScope<IRepository<BookingEntity>>();
+            var options = _serviceScopeFactory.GetServiceFromScope<IOptions<AutoClearOptions>>();
             while (!stoppingToken.IsCancellationRequested)
             {
                 var firstEntity = await bookingEntityRepository.FirstAsync();
                 if (firstEntity == null) continue;
                 await Offset(firstEntity);
+                await ClearOverdue(bookingEntityRepository, options);
             }
         }
 
@@ -42,20 +47,35 @@ namespace Bronistol.Core.HostedServices.PriorityService
 
             if (nextEntity.Priority.Priority > currentEntity.Priority.Priority)
             {
+                var offset = currentEntity.AssignedDate.Date - currentEntity.SubmitDate.Date;
                 currentEntity.SubmitDate.Date = nextEntity.AssignedDate.Date;
-                await bookingEntityRepository.UpdateAsync(currentEntity);
+                currentEntity.AssignedDate.Date = currentEntity.SubmitDate.Date + offset;
+                await bookingEntityRepository.UpdateAsync(nextEntity);
             }
             else if (nextEntity.Priority.Priority < currentEntity.Priority.Priority)
             {
+                var offset = nextEntity.AssignedDate.Date - nextEntity.SubmitDate.Date;
                 nextEntity.SubmitDate.Date = currentEntity.AssignedDate.Date;
+                nextEntity.AssignedDate.Date = nextEntity.SubmitDate.Date + offset;
                 await bookingEntityRepository.UpdateAsync(nextEntity);
             }
             else
             {
+
                 return;
             }
-
             await Offset(nextEntity);
+        }
+        private async Task ClearOverdue(IRepository<BookingEntity> repository, IOptions<AutoClearOptions> options)
+        {
+            var dateTimeNow = DateTime.UtcNow;
+            var bookingEntities = await repository
+                .WhereAsync(x => x.AssignedDate.Date
+                    .AddDays(options.Value.DaysLater) < dateTimeNow);
+            foreach (var bookingEntity in bookingEntities)
+            {
+                await repository.RemoveAsync(x => x.Id == bookingEntity.Id);
+            }
         }
     }
 }
---
+++ Bronistol.Core/Options/AutoClearOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace Bronistol.Core.Options
+{
+    public class AutoClearOptions
+    {
+        public int DaysLater { get; set; }
+    }
+}
