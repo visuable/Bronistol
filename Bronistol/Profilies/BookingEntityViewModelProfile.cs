c801df95277c476b6e3ed3f9e88923809baf14ae
n.korabelnikov@isoit.ru <n.korabelnikov@isoit.ru>
2021-04-06 18:55:04 +0300

    Added profile for Razor models

---
+++ Bronistol/Profilies/BookingEntityViewModelProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Bronistol.Models;
+
+namespace Bronistol.Profilies
+{
+    public class BookingEntityViewModelProfile : Profile
+    {
+        public BookingEntityViewModelProfile()
+        {
+            CreateMap<BookingEntityPageModel, BookingEntityViewModel>()
+                .ForPath(x => x.Name.Name, y => y.MapFrom(z => z.Name))
+                .ForPath(x => x.AssignedDate.Date, y => y.MapFrom(z => z.AssignedDate))
+                .ForPath(x => x.SubmitDate.Date, y => y.MapFrom(z => z.SubmitDate))
+                .ForPath(x => x.Note.Description, y => y.MapFrom(z => z.Note))
+                .ForPath(x => x.Reason.Description, y => y.MapFrom(z => z.Reason))
+                .ForPath(x => x.Priority.Priority, y => y.MapFrom(z => z.Priority));
+        }
+    }
+}
